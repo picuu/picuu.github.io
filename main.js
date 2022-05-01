2c3bfde7398b1896332461b69f679090a83d4ad6
Picuu <ltpicuuu@gmail.com>
2022-05-01 23:06:10 +0200

    feat: add show & hide menu function

---
+++ main.js
@@ -0,0 +1,18 @@
+/*==================== MENU SHOW & HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu"),
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu");
+    });
+}
+
+/*===== MENU HIDE =====*/
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu");
+    });
+}
