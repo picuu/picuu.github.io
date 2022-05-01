20ae4cf03fce9c539c4777002a387c13ae19b783
Picuu <ltpicuuu@gmail.com>
2022-05-01 23:08:53 +0200

    feat: improve (and add new) menu functions

--- main.js
+++ main.js
@@ -16,3 +16,35 @@ if (navClose) {
         navMenu.classList.remove("show-menu");
     });
 }
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
+    navMenu.classList.remove("show-menu");
+}
+navLink.forEach(n => n.addEventListener("click", linkAction));
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            const navEl = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+            navEl.classList.add("active-link");
+        } else {
+            const navEl = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+            navEl.classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
